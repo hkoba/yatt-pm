8412a3117e60989ef20015811e1bab66da795ab2
KOBAYASI, Hiroaki <buribullet@gmail.com>
2010-04-07 03:29:15 +0000

    #54 FollowSymLinks も必要
    git-svn-id: file:///home/svkroot/local/mirror/yatt-pm/trunk/yatt-pm@1474 3da9da48-f890-402d-a5f9-db82262da96b

--- apache/redhat-enable-cgi.pl
+++ apache/redhat-enable-cgi.pl
@@ -10,7 +10,7 @@ while (<>) {
     print STDERR $1 ? "Changed" : "Already OK", ": $2\n";
   } elsif ($line = m{^(<Directory "/var/www/cgi-bin">)} .. m{^</Directory}) {
     $context = $1 if $line == 1;
-    ensure_config("Options", "ExecCGI");
+    ensure_config("Options", "ExecCGI", "FollowSymLinks");
   } elsif ($line = m{^(<Directory "/var/www/html">)} .. m{^</Directory}) {
     $context = $1 if $line == 1;
     ensure_config("AllowOverride", "All");
@@ -20,17 +20,42 @@ while (<>) {
 }
 
 sub ensure_config {
-  my ($config, $expect) = @_;
-  my ($value) = /^\s*$config\b(.*)/
+  my $config = shift;
+  my ($indent, $value) = /^(\s*)$config\b(.*)/
     or return;
   if ($value =~ /None/) {
-    $value = $expect;
-  } elsif ($value !~ /$expect/) {
-    $value .= " $expect";
+    $value = join " ", @;
   } else {
-    print STDERR "Already OK: $context $_\n";
-    return;
+    my (%specified, @order);
+    foreach my $item (split " ", $value) {
+      my $relative = $item =~ s/^([\-\+])//;
+      $specified{$item} = do {
+	if (not $relative or $1 eq '+') {
+	  1
+	} elsif ($1 eq '-') {
+	  0
+	} else {
+	  die "really?? $_";
+	}
+      };
+      push @order, $item;
+    }
+    my $nchanges;
+    foreach my $expect (@_) {
+      if ($specified{$expect}) {
+	print STDERR "Already OK: $context $expect\n";
+	next;
+      }
+      push @order, $expect unless defined $specified{$expect};
+      $specified{$expect} = 1;
+      $nchanges++;
+    }
+    return unless $nchanges;
+    $_ = "$indent$config ". join " ", map {$specified{$_} ? "+$_" : "-$_"} @order;
+    print STDERR "Changed: $context $_\n";
   }
-  $_ = "$config $value";
-  print STDERR "Changed: $context $_\n";
 }
+
+__END__
+
+  perl -i.bak redhat-enable-cgi.pl /etc/httpd/conf/httpd.conf
