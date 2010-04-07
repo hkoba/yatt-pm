15407d31498addeb9420f3a779200b533633eca8
KOBAYASI, Hiroaki <buribullet@gmail.com>
2010-04-07 03:28:54 +0000

    #54 Cosmetic cleansing. (indentation)
    git-svn-id: file:///home/svkroot/local/mirror/yatt-pm/trunk/yatt-pm@1473 3da9da48-f890-402d-a5f9-db82262da96b

--- apache/redhat-enable-cgi.pl
+++ apache/redhat-enable-cgi.pl
@@ -2,34 +2,35 @@
 use strict;
 use warnings;
 
-my ($line, $context);
+our $context = "";
+my ($line);
 while (<>) {
   chomp;
   if (s/^(\#)?(AddHandler cgi-script \.cgi\b.*)$/$2/) {
     print STDERR $1 ? "Changed" : "Already OK", ": $2\n";
   } elsif ($line = m{^(<Directory "/var/www/cgi-bin">)} .. m{^</Directory}) {
     $context = $1 if $line == 1;
-    ensure_config("Options", "ExecCGI", $context);
+    ensure_config("Options", "ExecCGI");
   } elsif ($line = m{^(<Directory "/var/www/html">)} .. m{^</Directory}) {
     $context = $1 if $line == 1;
-    ensure_config("AllowOverride", "All", $context);
+    ensure_config("AllowOverride", "All");
   }
 } continue {
   print "$_\n";
 }
 
 sub ensure_config {
-  my ($config, $expect, $context) = @_;
-    my ($value) = /^\s*$config\b(.*)/
-      or return;
-    if ($value =~ /None/) {
-      $value = $expect;
-    } elsif ($value !~ /$expect/) {
-      $value .= " $expect";
-    } else {
-      print STDERR "Already OK: $context $_\n";
-      return;
-    }
-    $_ = "$config $value";
-    print STDERR "Changed: $context $_\n";
+  my ($config, $expect) = @_;
+  my ($value) = /^\s*$config\b(.*)/
+    or return;
+  if ($value =~ /None/) {
+    $value = $expect;
+  } elsif ($value !~ /$expect/) {
+    $value .= " $expect";
+  } else {
+    print STDERR "Already OK: $context $_\n";
+    return;
+  }
+  $_ = "$config $value";
+  print STDERR "Changed: $context $_\n";
 }
