3bccce51e890cd5f513ab957975078e794e332e0
KOBAYASI, Hiroaki <buribullet@gmail.com>
2010-04-08 03:37:22 +0000

    To skip rlimit test if BSD::Resource is missing
    git-svn-id: file:///home/svkroot/local/mirror/yatt-pm/trunk/yatt-pm@1508 3da9da48-f890-402d-a5f9-db82262da96b

--- web/cgi-bin/yatt.lib/YATT.pm
+++ web/cgi-bin/yatt.lib/YATT.pm
@@ -5,7 +5,7 @@ require 5.007_001; # For sprintf reordering.
 
 use strict;
 use warnings FATAL => qw(all);
-use version; our $VERSION = qv('0.0.3');
+use version; our $VERSION = qv('0.0.4');
 use File::Basename;
 
 BEGIN {
