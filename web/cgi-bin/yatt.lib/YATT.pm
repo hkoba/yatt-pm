acd64a8e522ad38ee56f9bbdb2f78b6577872c7a
KOBAYASI, Hiroaki <buribullet@gmail.com>
2013-05-18 11:21:47 +0900

    v0.0.8

--- web/cgi-bin/yatt.lib/YATT.pm
+++ web/cgi-bin/yatt.lib/YATT.pm
@@ -5,7 +5,7 @@ require 5.007_001; # For sprintf reordering.
 
 use strict;
 use warnings FATAL => qw(all);
-use version; our $VERSION = qv('0.0_8');
+use version; our $VERSION = qv('0.0.8');
 use File::Basename;
 
 BEGIN {
